91d47bd0b30baf52e7b54214446a142cdbe56683
김한비 <102509150+khbbbbi@users.noreply.github.com>
2023-05-23 08:52:06 +0000

    코드 예제

---
+++ 코드예제/(1)이메일 형식 검사(match 함수 사용).py
@@ -0,0 +1,9 @@
+import re
+
+email_regex = re.compile("[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+$")
+email_input = input("이메일을 입력하세요 : ")
+
+if email_regex.match(email_input):
+    print("적합")
+else:
+    print("부적합")
